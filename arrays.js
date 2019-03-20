c38d4f28a6468ac3fe7c2c78526c97ee4ee69d35
Jessica Pope <jessica.pope.co@gmail.com>
2019-03-20 15:25:28 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -7,4 +7,4 @@ function accessElementInArray(array, index){return array[index]}
 function destructivelyRemoveElementFromBeginningOfArray(array){array.shift();return array}
 function removeElementFromBeginningOfArray(array){array = array.slice(1); return array}
 function destructivelyRemoveElementFromEndOfArray(array){array.pop();return array}
-function removeElementFromEndOfArray(array){array = array.slice(array.length);return array}
+function removeElementFromEndOfArray(array){array = array.slice(-array.length);return array}
