dc6fb2ea232dd4bef097d3addcbf3deb6cdfddf5
Jessica Pope <jessica.pope.co@gmail.com>
2019-03-20 15:05:46 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -3,3 +3,4 @@ function addElementToBeginningOfArray(array, element){array = [element, ...array
 function destructivelyAddElementToBeginningOfArray(array, element){array.unshift(element); return array}
 function addElementToEndOfArray(array, element){array = [...array, element]; return array}
 function destructivelyAddElementToEndOfArray(array, element){array.push(element);return array}
+function accessElementInArray(array, index){return array[index]}
