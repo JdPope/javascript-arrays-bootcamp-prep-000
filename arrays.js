6592752f269eb45808fd049375ef1d9d64dd0cd9
Jessica Pope <jessica.pope.co@gmail.com>
2019-03-20 14:40:45 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -1,2 +1,3 @@
 var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"]
 function addElementToBeginningOfArray(array, element){array = [element, ...array]}
+function destructivelyAddElementToBeginningOfArray(array, element){array = array.unshift(element)}
